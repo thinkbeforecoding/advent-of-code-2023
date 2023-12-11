990a1fafa19a1a5b9ff3aaffdf0305292133dd85
jchassaing <jeremie.chassaing@gmail.com>
2023-12-12 00:54:36 +0100

    Comment day09

--- day09.fsx
+++ day09.fsx
@@ -5,18 +5,24 @@ let series =
     lines
     |> Array.map (fun l -> l.Split(' ') |> Array.map int)
 
-let rec predict (values: int[]) = 
+/// this one was quite easy.
+/// it uses a recursive function 
+let rec predict sum (values: int[]) = 
+    // compute the different of consecutive numbers
     let derived = values |> Seq.pairwise |> Seq.map (fun (x, y) -> y-x) |> Seq.toArray
+    // and we must sum last numbers
     if derived |> Seq.forall ((=) 0) then
-        values[values.Length-1]
+        sum + values[values.Length-1]
     else
-        values[values.Length-1] + predict derived
+        // sum this level last numbers with previous
+        predict (sum + values[values.Length-1])  derived
 
 
 series
-|> Array.map predict
+|> Array.map (predict 0)
 |> Array.sum
 
+// this one is easier to write without tail recursion
 let rec predict2 (values: int[]) = 
     let derived = values |> Seq.pairwise |> Seq.map (fun (x, y) -> y-x) |> Seq.toArray
     if derived |> Seq.forall ((=) 0) then
