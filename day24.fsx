c39bba9f6eecf10caae02412f266f76c74958ce1
jchassaing <jeremie.chassaing@gmail.com>
2023-12-28 02:59:26 +0100

    Comment day24/day25

--- day24.fsx
+++ day24.fsx
@@ -1,13 +1,11 @@
 
 open System
 
+// day24 Part 2 was especialy not easy
+// but part 1 was ok
 let input =  IO.File.ReadAllLines("./input/day24.txt")
-//     """19, 13, 30 @ -2,  1, -2
-// 18, 19, 22 @ -1, -1, -2
-// 20, 25, 34 @ -2, -2, -4
-// 12, 31, 28 @ -1, -2, -1
-// 20, 19, 15 @  1, -5, -3""".Split '\n'
 
+// parse the vectors
 let parseVec (l:String) = 
     match l.Split(',', StringSplitOptions.TrimEntries) with
     | [| x;y;z |] -> decimal x, decimal y, decimal z
@@ -18,34 +16,46 @@ let parse (l: string) =
     | _ -> failwith "invalid line"
 
 
-
+// take vectors and keep only x,y
 let hails = input |> Array.map parse |> Array.map (fun ((x,y,_),(vx,vy,_)) -> (x,y),(vx,vy))
 
-
+// a few functions to work with the 2D vectors
+// check if vector is vertical
 let isVertical (_,(vx,_)) = vx = 0m
+// take x from position
 let x0 ((x,_),_) = x
+// take y from position
 let y0 ((_,y),_) = y
-
+// take x from speed
 let vx (_,(vx,_)) = vx
+// take y from speed
 let vy (_,(_, vy)) = vy
 
+// compute the slop of the line
 let slope (_,(vx,vy)) = vy / vx
 
+// compute the y value at x' on the line
 let value x' ((x,y),(vx,vy)) = vy / vx * (x'-x) + y
 
-
+// check whether lines defined by h1 and h2 intersect in the future
 let intersect h1 h2 =
     if isVertical h1 then
+        // h1 is vertical
         if isVertical h2 then
+            // h2 is also vertical so they are parallel
             None
         else
+            // h2 will meet h1, at h1 x (all points are at x since it's vertical)
             let x = x0 h1 
             let y = value x h2
+            // compare signs of vectors from origin to interection to check if the 
+            // intersection is in the future for both
             if sign (vy h1) = sign (y - y0 h1) && sign (vy h2) = sign (y - y0 h2) then
                 Some (x, y)
             else
                 None
     elif isVertical h2 then
+        // h2 is vertical, same as above
         let x = x0 h2
         let y = value x h1
         if sign (vy h1) = sign (y - y0 h1) && sign (vy h2) = sign (y - y0 h2) then
@@ -55,14 +65,17 @@ let intersect h1 h2 =
     else
         // y = a (x-x0) + y0
         // y = a x + y0 - a x0
+        // compute both slopes
         let a= slope h1
         let b = y0 h1 - a * x0 h1
         let a'= slope h2
         let b' = y0 h2 - a' * x0 h2
 
         if a = a' then
+            // lines are parralel
             None
         else
+            // compute the coordinates of the interesection
             // y = a x + b
             // y = a' x + b'
             // 0 = (a - a') x + (b - b')
@@ -70,18 +83,18 @@ let intersect h1 h2 =
             let x = (b' - b) / (a - a')
             let y = a * x + b
             
+            // check if the intersection is in the future for both
             if sign (vx h1) = sign (x - x0 h1) &&
                 sign (vx h2) = sign (x - x0 h2) then
                 Some (x,y)
             else
                 None
 
-// let area = (7m,7m),(27m,27m)
-
 let area = (200000000000000m, 200000000000000m),(400000000000000m, 400000000000000m)
-
+// check if a point is in the area
 let inArea ((x0,y0),(x1,y1)) (x,y) = x >= x0 && x <= x1 && y >= y0 && y <= y1
 
+// compute all intersections and keep the ones in the area
 [ for i in 0 .. hails.Length-1 do
     for j in i+1 .. hails.Length-1 do
         intersect hails[i] hails[j] ]
@@ -89,16 +102,19 @@ let inArea ((x0,y0),(x1,y1)) (x,y) = x >= x0 && x <= x1 && y >= y0 && y <= y1
 |> List.length
 
 
+// part 2
 
+// using the 3 first lines we can make a system of 9 equations with 6 variables:
+// x,y,z vx,vy,vy
+// it generates a program for a z3 solver like https://jfmc.github.io/z3-play/
 
+// parse the lines, but in 3D
 let hails3d = input |> Array.map parse
 
-
-
-let inline ( *^ ) (u1,u2,u3) (v1,v2,v3)  = u2*v3-u3*v2, u3*v1 - u1*v3, u1*v2 - u2*v1 
-let inline ( *. ) (u1,u2,u3) (v1,v2,v3)  = u1*v1 + u2 *v2 + u3 * v3
-
-
+// for each line we can write
+// (x0 - x) * (vy - vy0) = (y0 - y) * (vx - vx0)
+// (y0 - y) * (vz - vz0) = (z0 - z) * (vy - vy0)
+// as z3 asserts
 let c = 3
 let equations = 
     hails3d |> Array.take c |> Array.mapi (fun i ((x,y,z),(vx,vy,vz)) ->
@@ -107,6 +123,7 @@ let equations =
                  |]
     ) |> Array.concat |> String.concat "\n"
 
+// the variable declarations (they are all Int)
 let defs= 
     [ "(declare-const x Int)"
       "(declare-const vx Int)"
@@ -116,7 +133,8 @@ let defs=
       "(declare-const vz Int)"
     ] |> String.concat "\n"
 
-
+// print the program
 printfn $"{defs}\n{equations}\n(check-sat)\n(get-model)"
 
+// the three position coordinates in the solution are:
 140604613634294L + 224390889669946L + 206098283112689L
--- day25.fsx
+++ day25.fsx
@@ -3,20 +3,11 @@
 open System
 open System.Collections.Generic
 
+// This one was also a bit tricky,
+// if there was only one bridge, there is an algorithm to find cut bridges
+// but here, there are three, and we must use the max flow / min cut algorithm
+// https://en.wikipedia.org/wiki/Max-flow_min-cut_theorem
 let input = IO.File.ReadAllLines "./input/day25.txt"
-//     """jqt: rhn xhk nvd
-// rsh: frs pzl lsr
-// xhk: hfx
-// cmg: qnr nvd lhk bvb
-// rhn: xhk bvb hfx
-// bvb: xhk hfx
-// pzl: lsr hfx nvd
-// qnr: nvd
-// ntq: jqt hfx bvb xhk
-// nvd: lhk
-// lsr: lhk
-// rzs: qnr cmg lsr rsh
-// frs: qnr lhk lsr""".Split '\n'
 
 let parse (l: string) =
     let parts = l.Split ' '
@@ -24,10 +15,10 @@ let parse (l: string) =
     [ for p in Seq.skip 1 parts do 
         source,p ]
 
+// read all wires definitions and flaten the list
 let wires = input |> Seq.collect parse |> Seq.toList 
-wires |> List.map(fun (s,e) -> $"{s}-{e}") |> fun l -> IO.File.WriteAllLines("out25.txt", l)
 
-
+// make a list of all connections in both directions
 let connections =
     [ for s,e in wires do 
         s,e
@@ -36,38 +27,42 @@ let connections =
     |> List.map (fun (s,l) -> s, List.map snd l)
     |> Map.ofList
     
+// reorder ends min -> max 
 let order s e = min s e, max s e
+// all edges in the graph
 let edges = 
     [ for s,e in wires do
         order s e ]
     |> set
 
+// make a set of all components (vertices)
 let components = connections.Keys |> set
-components.Count
 
 
-let source = components.MinimumElement
-let sink = components.MaximumElement
-
-
+// find a path to target in graph g, visited is used to avoid going twice on the same vertex
 let rec findPath target g (visited: _ HashSet ) (queue: _ Queue)  =
     match queue.TryDequeue() with
-    | false,_ -> None
+    | false,_ -> None // no more path to process, there is no path to target
     | true, (v, path)  ->
         if v = target then
+            // we found the path
+            // add the last point and reverse the list as it has been constructed in
+            // other direction
             Some ( List.rev (v :: path) )
         elif visited.Contains v then
+            // the vertex has been visited, try something else
             findPath target g visited queue
         else
+            // add vertex to visited
             visited.Add(v) |> ignore
+            // add all connected vertices to the queue to explore
             for n in connections[v] do
                 if Set.contains (order n v) g then
                     queue.Enqueue(n, v :: path)
                
             findPath target g visited queue 
 
-
-
+// use find path to actually compute a path from source to sink in graph g
 let path source sink g = 
     let queue = Queue()
     queue.Enqueue(source,[])
@@ -76,20 +71,22 @@ let path source sink g =
     | Some p ->
         p |> List.pairwise
 
-path source sink edges
-
-edges.Count
-
+// add n to the flow of edge e
 let addFlow e n f =
     match Map.tryFind e f with
     | None -> Map.add e n f
     | Some v -> Map.add e (v+n) f
 
+// compute the max flow.
+// if source and sink are in the two subgraphs, the flow should be 3 as there are 3 bridges
 let rec maxFlow source sink g (flow: Map<string*string, int>) paths mf =
-
+    // find a path from source to sink in the residual graph g (the graph with saturated edges removed)
     match path source sink g  with
-    | [] -> flow, paths, mf, g
+    | [] -> 
+        // no more path from source to sink, we have the max flow
+        flow, paths, mf, g
     | p ->
+        // compute the new flow on the path (add 1 to each edge on the path) 
         let newFlow = 
             (flow,p) ||> List.fold (fun f (s,t) ->
                 f
@@ -97,22 +94,22 @@ let rec maxFlow source sink g (flow: Map<string*string, int>) paths mf =
                 |> addFlow (t,s) -1
             )
 
+        // prepare the new residual graph by removing edges on the path
         let newg = g - (p |> List.map (fun (s,e) -> order s e) |> set)
+        // add the path to paths, and increase maxflow
         maxFlow source sink newg newFlow (p :: paths) (mf + 1)
 
 let componentsl = Set.toList components
-
-
+// take a random source and sink/target
 let s = componentsl[Random.Shared.Next(componentsl.Length)]
 let t = componentsl[Random.Shared.Next(componentsl.Length)]
 let source = s
 let sink = t
+// compute the flow (if it's not 3, the source and target are on the same side of the bridges, try again)
 let f, ps, mf, rg = maxFlow source sink edges Map.empty [] 0
 
-let vs = [ for s,e in rg do
-            s
-            e ] |> set
 
+// make a list of all vertices reachable from source in the residual graph
 let rec reachable' g (visited: _ HashSet ) (queue: _ Queue)  =
     match queue.TryDequeue() with
     | false,_ -> visited
@@ -132,24 +129,26 @@ let reachable g source =
     q.Enqueue(source)
     reachable' g (HashSet()) q
 
-
+// rs is the set of reachable vertices
 let rs = reachable rg source
 
-rs
-
+// the cut is on the edges where one end is reachable (in rs) and the other is not
 let cut =
     [ for s,e in edges do
         if rs.Contains s && not (rs.Contains e) || rs.Contains e && not (rs.Contains s) then
             order s e ] |> set
+
+// remove cut edges from the full graph
 let split = edges - cut
 
 
 
-
+// now, the set of reachable points from source contains all the vertices from one side
 let countSource = reachable split source |> Seq.length
+// the set of reachable points from from sink contains all vertices from the other side
 let countSink = reachable split sink |> Seq.length
 
-
+// multiply both counts
 countSource * countSink
 
 
